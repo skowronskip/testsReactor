c28bdd40c0c8d85657b3501e8b91f5dd85448db1
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-05-16 17:01:31 +0200

    Test when amount is bigger than zero, proper payment is returned

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -1,15 +1,46 @@
 package edu.iis.mto.testreactor.exc3;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 public class AtmMachineTest {
+
+    private CardProviderService cardProviderService;
+    private BankService bankService;
+    private MoneyDepot moneyDepot;
+
+    @Before
+    public void setUp() throws CardAuthorizationException, InsufficientFundsException, MoneyDepotException {
+        cardProviderService = mock(CardProviderService.class);
+        bankService = mock(BankService.class);
+        moneyDepot = mock(MoneyDepot.class);
+        when(cardProviderService.authorize(any(Card.class))).thenReturn(AuthenticationToken.builder().withUserId("userId").build());
+        doNothing().when(bankService).startTransaction(any(AuthenticationToken.class));
+        doNothing().when(bankService).charge(any(AuthenticationToken.class), any(Money.class));
+        doNothing().when(bankService).commit(any(AuthenticationToken.class));
+        doNothing().when(moneyDepot).releaseBanknotes(anyListOf(Banknote.class));
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
+    }
+
+    @Test
+    public void whenAmountIsBiggerThanZero_thenProperPaymentIsReturned() {
+        AtmMachine atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
+        Money money = Money.builder().withAmount(200).withCurrency(Currency.PL).build();
+        Card card = Card.builder().build();
+        Payment payment = atmMachine.withdraw(money, card);
+        assertEquals(payment.getValue().size(), 1);
+        assertEquals(payment.getValue().get(0), Banknote.PL200);
     }
 
 }
